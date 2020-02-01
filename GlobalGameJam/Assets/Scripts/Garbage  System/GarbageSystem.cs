a3580c85f6221c58e8282cfa0327dd4ba89ccb4d
LadyMarina <sharkk244@gmail.com>
2020-02-01 11:30:50 +0100

    garbage

--- GlobalGameJam/Assets/Scripts/Garbage  System/GarbageSystem.cs
+++ GlobalGameJam/Assets/Scripts/Garbage  System/GarbageSystem.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GarbageSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private InputMaster controls;
+    private float x;
+    private void OnEnable()
     {
-        
+        controls.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        controls.Disable();
     }
+    private void Awake()
+    {
+        controls = new InputMaster();
+        controls.Player1.Interact.started += ctx => x = ctx.ReadValue<float>();
+        controls.Player1.Interact.canceled += ctx => x = 0;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && x == 1)
+        {
+            if (other.transform.GetComponentInChildren<PlayerInventary>().playerHaveAObject == true)
+            {
+                Debug.Log("DESTROY");
+                other.transform.GetComponentInChildren<PlayerInventary>().playerHaveAObject = false;
+                Destroy(other.GetComponentInChildren<PlayerInventary>().GetComponentsInChildren<Transform>()[1].gameObject);
+            }
+            
+         
+        }
+    }
+
+   
 }
--- GlobalGameJam/Assets/Scripts/Player/PlayerInventary.cs
+++ GlobalGameJam/Assets/Scripts/Player/PlayerInventary.cs
@@ -14,6 +14,6 @@ public class PlayerInventary : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        Debug.Log(playerHaveAObject);
     }
 }
