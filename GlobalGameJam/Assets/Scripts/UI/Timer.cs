e347797ec9630414082407e8bf4256ebce912d84
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-02-02 12:14:43 +0100

    Ho

---
+++ GlobalGameJam/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public int minuts;
+    public float seconds;
+
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        text.text = minuts + ":" + seconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+
+        if(seconds <= 0)
+        {
+            minuts--;
+            seconds = 59;
+        }
+
+        if(minuts == 0)
+        {
+            
+            text.text = 0 + minuts + ":" + seconds.ToString("F2");
+        }
+        else
+        {
+            text.text = minuts + ":" + seconds.ToString("F0");
+        }
+        
+    }
+}
